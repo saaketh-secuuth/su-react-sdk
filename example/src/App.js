80336df696aac12fe360c460d971124179139e5e
saaketh <saakethaj@gmail.com>
2021-09-15 16:43:53 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-secuuth'
+import 'react-secuuth/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
